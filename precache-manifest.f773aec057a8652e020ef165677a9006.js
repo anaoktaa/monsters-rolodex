b3482dd54ca45531ac1e9f61db70c3960d4b86d2
@anaokta <ana.voltras@co.id>
2019-12-22 20:24:17 +0700

    Updates

---
+++ precache-manifest.f773aec057a8652e020ef165677a9006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e82e087d7cef5522375e99171d0218f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3ff4610cdc7c03846630",
+    "url": "/monsters-rolodex/static/css/main.339a418a.chunk.css"
+  },
+  {
+    "revision": "1b43d2eaa999d2a50f77",
+    "url": "/monsters-rolodex/static/js/2.c698f0ee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.c698f0ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ff4610cdc7c03846630",
+    "url": "/monsters-rolodex/static/js/main.9c733a40.chunk.js"
+  },
+  {
+    "revision": "366f143aac14b6e58293",
+    "url": "/monsters-rolodex/static/js/runtime-main.1427f30c.js"
+  }
+]);
